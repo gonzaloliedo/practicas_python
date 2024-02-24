45ca822c4ba2e61609f7ba8867bb1c8f3c2fb943
gliedo <liedogonzalo@gmail.com>
2024-02-24 13:57:12 -0600

    versión 1

---
+++ Tuplas.py
@@ -0,0 +1,23 @@
+# Video 10 Tuplas
+
+# Se caracterizan por ser estrucuturas de datos inmutables, por lo tanto se usan para proteger datos. También son más eficientes que las listas.
+
+tupla = ("Juan", 2)
+
+#tupla[0]="Juana" #marca error porque no se puede modificar
+
+#Una opción es convertir la tupla en lista
+
+lista = list(tupla)
+
+print(lista)
+
+lista[0]="Juana"
+
+print(lista)
+
+# de manera recíproca la lista pasa a ser tupla
+
+tupla_modificada = tuple(lista)
+
+print(tupla_modificada)
